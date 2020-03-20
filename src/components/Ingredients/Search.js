e86885a3716eab928ee0610df0ea223cf05126b9
bsd <datta.bathina15@gmail.com>
2020-03-21 00:20:44 +0530

    Search functionality added by use effect.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [enteredFilterText, setEnteredFilterText] = useState('');
+  const { onLoadFilteredIngredients } = props;
+  useEffect(() => {
+    const query = enteredFilterText.length === 0 ?
+      '' :
+      `?orderBy="title"&equalTo="${enteredFilterText}"`;
+    fetch('https://react-hooks-tutorial-de828.firebaseio.com/ingredients.json' + query)
+      .then(response => response.json())
+      .then(responseData => {
+        let fetchedIngredients = [];
+        for (const ingredientKey in responseData) {
+          fetchedIngredients.push({
+            id: ingredientKey,
+            title: responseData[ingredientKey].title,
+            amount: responseData[ingredientKey].amount
+          });
+        }
+        onLoadFilteredIngredients(fetchedIngredients);
+      });
+  }, [enteredFilterText, onLoadFilteredIngredients])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilterText} onChange={event => setEnteredFilterText(event.target.value)} />
         </div>
       </Card>
     </section>
