354d60369a4455e8531c76a9f57512f782072b9c
bsd <datta.bathina15@gmail.com>
2020-03-21 00:39:38 +0530

    Used useRef, setTimeout to limit calls to backend.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,30 +6,36 @@ import './Search.css';
 const Search = React.memo(props => {
   const [enteredFilterText, setEnteredFilterText] = useState('');
   const { onLoadFilteredIngredients } = props;
+  const inputRef = useRef();
   useEffect(() => {
-    const query = enteredFilterText.length === 0 ?
-      '' :
-      `?orderBy="title"&equalTo="${enteredFilterText}"`;
-    fetch('https://react-hooks-tutorial-de828.firebaseio.com/ingredients.json' + query)
-      .then(response => response.json())
-      .then(responseData => {
-        let fetchedIngredients = [];
-        for (const ingredientKey in responseData) {
-          fetchedIngredients.push({
-            id: ingredientKey,
-            title: responseData[ingredientKey].title,
-            amount: responseData[ingredientKey].amount
+    const filterTimer = setTimeout(() => {
+      if (enteredFilterText === inputRef.current.value) {
+        const query = enteredFilterText.length === 0 ?
+          '' :
+          `?orderBy="title"&equalTo="${enteredFilterText}"`;
+        fetch('https://react-hooks-tutorial-de828.firebaseio.com/ingredients.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            let fetchedIngredients = [];
+            for (const ingredientKey in responseData) {
+              fetchedIngredients.push({
+                id: ingredientKey,
+                title: responseData[ingredientKey].title,
+                amount: responseData[ingredientKey].amount
+              });
+            }
+            onLoadFilteredIngredients(fetchedIngredients);
           });
-        }
-        onLoadFilteredIngredients(fetchedIngredients);
-      });
-  }, [enteredFilterText, onLoadFilteredIngredients])
+      }
+    }, 500);
+    return () => clearTimeout(filterTimer);
+  }, [enteredFilterText, onLoadFilteredIngredients, inputRef])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilterText} onChange={event => setEnteredFilterText(event.target.value)} />
+          <input ref={inputRef} type="text" value={enteredFilterText} onChange={event => setEnteredFilterText(event.target.value)} />
         </div>
       </Card>
     </section>
